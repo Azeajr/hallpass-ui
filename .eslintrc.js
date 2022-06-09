8d8fb391fbed20a13ed772d313346ff4464113fe
Antonio Zea Jr <zeajantonio@gmail.com>
2022-06-08 23:21:58 -0400

    eslint and prettier fix 2
    Cleaned up branch to remove the previous ejection of webpack.
    Then fixed eslint and prettier.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,48 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["plugin:react/recommended", "airbnb"],
-  parser: "@typescript-eslint/parser",
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'react-app',
+    'react-app/jest',
+    'plugin:testing-library/react',
+    'plugin:jest-dom/recommended',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  plugins: ["react", "@typescript-eslint"],
-  rules: {},
+  plugins: [
+    'testing-library',
+    'jest-dom',
+    'prettier',
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [
+      2,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': { node: { extensions: ['.js', '.jsx', '.ts', '.tsx'] } },
+  },
 };
