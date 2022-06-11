2dc7b1cb82d0baf30f0ea5b5ad9621f74d3bdedc
Antonio Zea Jr <zeajantonio@gmail.com>
2022-06-10 22:41:41 -0400

    refactor Main to be entry point

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,9 @@
-import { Box } from '@mui/material';
+// import r from 'rethinkdb';
+import Dashboard from '../components/dashboard/Dashboard';
+import teacherData from '../data/teacherData.json';
 
 function Main() {
-  return <Box>Hello</Box>;
+  return <Dashboard dashboardTitle="HallPass" teacherData={teacherData} />;
 }
 
 export default Main;
