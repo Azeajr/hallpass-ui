fb03cc4c0c482d8a8119c813bb426dff80ed361a
Antonio Zea Jr <zeajantonio@gmail.com>
2022-07-19 18:55:45 -0400

    adds selections array to props

--- src/components/DropdownFormInput/DropdownFormInput.tsx
+++ src/components/DropdownFormInput/DropdownFormInput.tsx
@@ -4,7 +4,11 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 
-function DropdownFormInput() {
+interface Props {
+  selections: string[];
+}
+
+function DropdownFormInput({ selections }: Props) {
   const [age, setAge] = React.useState('');
 
   const handleChange = (event: SelectChangeEvent) => {
@@ -26,9 +30,9 @@ function DropdownFormInput() {
           <MenuItem value="">
             <em>None</em>
           </MenuItem>
-          <MenuItem value={10}>Twenty</MenuItem>
-          <MenuItem value={21}>Twenty one</MenuItem>
-          <MenuItem value={22}>Twenty one and a half</MenuItem>
+          {selections.map((selection) => (
+            <MenuItem value={selection}>{selection}</MenuItem>
+          ))}
         </Select>
       </FormControl>
     </div>
