56d307eb17ed66f7c22b1b0034736209c130bb6b
Antonio Zea Jr <zeajantonio@gmail.com>
2022-08-09 10:06:41 -0400

    add uncommitted work

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,9 +1,23 @@
 // import r from 'rethinkdb';
+import { Button } from '@mui/material';
+import Axios from 'axios';
 import Dashboard from '../components/Dashboard/Dashboard';
 import teacherData from '../data/teacherData.json';
 
 function Main() {
-  return <Dashboard dashboardTitle="HallPass" teacherData={teacherData} />;
+  return (
+    <>
+      <Dashboard dashboardTitle="HallPass" teacherData={teacherData} />
+      <Button
+        onClick={async () => {
+          const data = await Axios.get('http://localhost:3002/api/get');
+          console.log(data);
+        }}
+      >
+        apiTest
+      </Button>
+    </>
+  );
 }
 
 export default Main;
